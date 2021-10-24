bbec045c29d12e411ac0e8883234ce1faef2e851
Muhammad Usama <51174487+Osama-Younas@users.noreply.github.com>
2021-10-24 16:37:23 +0500

    Add files via upload

---
+++ Logout.php
@@ -0,0 +1,40 @@
+<?php
+
+include "Database.php";
+
+class Logout_user extends Database
+{
+
+ function logout($email)
+ {
+    $data = json_decode(file_get_contents("php://input"),true);
+    $email = $data['email'];
+    $status="INACTIVE";
+    $token="Null";
+    $db_connection = new Database();
+    $conn = $db_connection->build_connection(); 
+
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
+        $message_display=array("Status_code"=>422,"Message"=>'Invalid Email pattern');//status code 422 because user enter invalid email
+        print_r(json_encode($message_display));
+        http_response_code(422);  
+    }
+    else
+    {
+        $q = "UPDATE merchant SET Token = '{$token}' , m_status ='{$status}' where Email ='{$email}'";//Updating value of token and status
+        $result = mysqli_query($conn,$q) or die("SQL QUERY FAIL.");
+        $message_display=array("Status_code"=>200,"Message"=>'Logout');//status code 422 because user enter invalid email
+        print_r(json_encode($message_display));
+        http_response_code(200);  
+
+    }  
+ }
+
+}
+$Email = null;
+$Logout = new Logout_user();
+$Logout->logout($Email);
+?>
+
+
