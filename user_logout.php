21585cc59e639e942c2cd719dc6203b8d0f045e8
Muhammad Usama <51174487+Osama-Younas@users.noreply.github.com>
2021-10-25 22:47:10 +0500

    user_logout_api(final)

---
+++ user_logout.php
@@ -0,0 +1,44 @@
+<?php
+
+include "Database.php";
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+class Logout_user extends Database
+{
+ function logout($key)
+ {
+    $data = json_decode(file_get_contents("php://input"),true);
+    $db_connection = new Database();
+    $conn = $db_connection->build_connection(); 
+    $key = $data['token'];
+    if(isset($key))
+    {
+        $query ="Select * from secondary_user where token ='{$key}'";
+        $result = $conn->query($query);
+      
+        if(mysqli_num_rows($result)>0)
+        {
+            $get_data = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            $id = $get_data['id'];
+            $status="INACTIVE";
+            $token="Null";
+
+
+            $q = "UPDATE secondary_user SET token = '{$token}' , u_status ='{$status}' where  id='{$id}'";//Updating value of token and status
+            $result = mysqli_query($conn,$q) or die("SQL QUERY FAIL.");
+            $message_display=array("Status_code"=>200,"Message"=>'Logout');//status code 422 because user enter invalid email
+            print_r(json_encode($message_display));
+            http_response_code(200);  
+        }
+    }  
+ }
+
+}
+
+$Logout = new Logout_user();
+$Logout->logout($key);
+?>
+
+
