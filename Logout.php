ba3f7d650a57df9db2bd2b489e4954617669f64c
Muhammad Usama <51174487+Osama-Younas@users.noreply.github.com>
2021-10-25 22:48:36 +0500

    merchant_logout_api(final)

--- Logout.php
+++ Logout.php
@@ -5,38 +5,40 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-class Logout_user extends Database
+class Logout_marchent extends Database
 {
- function logout($email)
+ function logout($key)
  {
     $data = json_decode(file_get_contents("php://input"),true);
-    $email = $data['email'];
-    $status="INACTIVE";
-    $token="Null";
     $db_connection = new Database();
     $conn = $db_connection->build_connection(); 
-
-    if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+    $key = $data['token'];
+    if(isset($key))
     {
-        $message_display=array("Status_code"=>422,"Message"=>'Invalid Email pattern');//status code 422 because user enter invalid email
-        print_r(json_encode($message_display));
-        http_response_code(422);  
-    }
-    else
-    {
-        $q = "UPDATE merchant SET Token = '{$token}' , m_status ='{$status}' where Email ='{$email}'";//Updating value of token and status
-        $result = mysqli_query($conn,$q) or die("SQL QUERY FAIL.");
-        $message_display=array("Status_code"=>200,"Message"=>'Logout');//status code 422 because user enter invalid email
-        print_r(json_encode($message_display));
-        http_response_code(200);  
+        $query ="Select * from merchant where Token ='{$key}'";
+        $result = $conn->query($query);
+      
+        if(mysqli_num_rows($result)>0)
+        {
+            $get_data = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            $id = $get_data['Id'];
+            $status="INACTIVE";
+            $token="Null";
 
+
+            $q = "UPDATE merchant SET Token = '{$token}' , m_status ='{$status}' where  id='{$id}'";//Updating value of token and status
+            $result = mysqli_query($conn,$q) or die("SQL QUERY FAIL.");
+            $message_display=array("Status_code"=>200,"Message"=>'Logout');//status code 422 because user enter invalid email
+            print_r(json_encode($message_display));
+            http_response_code(200);  
+        }
     }  
  }
 
 }
-$Email = null;
-$Logout = new Logout_user();
-$Logout->logout($Email);
+
+$Logout = new Logout_marchent();
+$Logout->logout($key);
 ?>
 
 
